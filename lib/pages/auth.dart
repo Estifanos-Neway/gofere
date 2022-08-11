7813f6dbe6c97c9ec122adfa100ba6302001c8dc
Estifanos Neway <estifanos.neway.d@gmail.com>
2022-08-12 02:11:44 +0300

    s

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -11,8 +11,171 @@ class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Auth Page"),
+      body: Container(
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <
+                Widget>[
+          DefaultTabController(
+            length: 2,
+            initialIndex: 0,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: TabBar(
+                    labelColor: Colors.blue,
+                    unselectedLabelColor: Colors.black,
+                    indicatorColor: Colors.transparent,
+                    tabs: [
+                      Tab(text: 'Sign in'),
+                      Tab(text: 'Sign up'),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 400, //height of TabBarView
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(
+                        color: Colors.grey,
+                        width: 0.5,
+                      ),
+                    ),
+                  ),
+                  child: TabBarView(
+                    children: <Widget>[
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Sign in to your account",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                hintText: "Email or Username",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(12),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                hintText: "Password",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(12),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Container(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: Text(
+                                  "Log in",
+                                  style: TextStyle(fontWeight: FontWeight.w300),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      Color.fromARGB(255, 2, 120, 217),
+                                  padding: EdgeInsets.symmetric(vertical: 18),
+                                  elevation: 0,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Checkbox(
+                                      value: false,
+                                      onChanged: (newValue) {},
+                                    ),
+                                    Text("Remamber me"),
+                                  ],
+                                ),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Forget Password?",
+                                    style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 40,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Divider(),
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Text("or sign in with"),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Expanded(child: Divider()),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 40,
+                            ),
+                            
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(
+                            'Display Tab 2',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]),
       ),
     );
   }

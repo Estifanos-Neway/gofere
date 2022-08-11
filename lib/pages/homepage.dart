e46730b75542a7301fe53ab0114aaecadcbf6886
Beka Dessalegn <bekadessalegn@gmail.com>
2022-08-12 02:10:31 +0300

    my commit

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,7 +1,9 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:gofere_travels/data/top_destinations.dart';
 import 'package:gofere_travels/widgets/homeFirst.dart';
 import 'package:gofere_travels/widgets/recommendedContainer.dart';
+import 'package:gofere_travels/widgets/recommendedForYou.dart';
 import 'package:gofere_travels/widgets/specialOffers.dart';
 import 'package:gofere_travels/widgets/special_offer_container.dart';
 
@@ -26,6 +28,7 @@ class _HomePageState extends State<HomePage> {
           children: [
             HomeFirst(),
             SpecialOffers(),
+            RecommendedForYou(),
             Container(
               height: MediaQuery.of(context).size.height - 140,
               width: MediaQuery.of(context).size.width,
@@ -34,86 +37,20 @@ class _HomePageState extends State<HomePage> {
                 children: [
                   Center(
                     child: Container(
-                      margin: EdgeInsets.only(top: 10, left: 20, right: 20),
-                      child: Text("Recommended for you",
+                      margin: EdgeInsets.only(top: 10),
+                      child: Text("Top destinations",
                         style: TextStyle(
-                            fontSize: 35,
-                            color: Colors.black87,
+                            fontSize: 30,
+                            color: Colors.black,
                             fontWeight: FontWeight.w500
                         ),
                         textAlign: TextAlign.center,
                       ),
                     ),
                   ),
-                  SizedBox(height: 15,),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ElevatedButton(
-                            onPressed: (){},
-                            style: ElevatedButton.styleFrom(
-                              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8)
-                              )
-                            ),
-                            child: Text("Hotel", style: TextStyle(
-                              fontSize: 16
-                            ),),
-                          ),
-                        ElevatedButton(
-                          onPressed: (){},
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.white,
-                              onPrimary: Colors.black87,
-                              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                // side: BorderSide(color: Colors.black, width: 0.2)
-                              )
-                          ),
-                          child: Text("Tour", style: TextStyle(
-                              fontSize: 16
-                          ),),
-                        ),
-                        ElevatedButton(
-                          onPressed: (){},
-                          style: ElevatedButton.styleFrom(
-                              primary: Colors.white,
-                              onPrimary: Colors.black87,
-                              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8)
-                              )
-                          ),
-                          child: Text("Rental", style: TextStyle(
-                              fontSize: 16
-                          ),),
-                        ),
-                        ElevatedButton(
-                          onPressed: (){},
-                          style: ElevatedButton.styleFrom(
-                              primary: Colors.white,
-                              onPrimary: Colors.black87,
-                              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8)
-                              )
-                          ),
-                          child: Text("Car", style: TextStyle(
-                              fontSize: 16
-                          ),),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 20,),
-                  RecommendedContainer(context),
                 ],
               ),
-            )
+            ),
           ],
         ),
       )
---
+++ lib/widgets/recommendedForYou.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:gofere_travels/widgets/recommendedContainer.dart';
+
+class RecommendedForYou extends StatefulWidget {
+  const RecommendedForYou({Key? key}) : super(key: key);
+
+  @override
+  State<RecommendedForYou> createState() => _RecommendedForYouState();
+}
+
+class _RecommendedForYouState extends State<RecommendedForYou> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(top: 10, left: 20, right: 20),
+              child: Text("Recommended for you",
+                style: TextStyle(
+                    fontSize: 35,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w500
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          SizedBox(height: 15,),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)
+                      )
+                  ),
+                  child: Text("Hotel", style: TextStyle(
+                      fontSize: 16
+                  ),),
+                ),
+                ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onPrimary: Colors.black87,
+                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        // side: BorderSide(color: Colors.black, width: 0.2)
+                      )
+                  ),
+                  child: Text("Tour", style: TextStyle(
+                      fontSize: 16
+                  ),),
+                ),
+                ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onPrimary: Colors.black87,
+                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)
+                      )
+                  ),
+                  child: Text("Rental", style: TextStyle(
+                      fontSize: 16
+                  ),),
+                ),
+                ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onPrimary: Colors.black87,
+                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)
+                      )
+                  ),
+                  child: Text("Car", style: TextStyle(
+                      fontSize: 16
+                  ),),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20,),
+          GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
+                childAspectRatio: (180 / 320),),
+              itemCount: 5,
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemBuilder: (context, index){
+                return Container(
+                    child: RecommendedContainer(context));
+              }),
+        ],
+      ),
+    );
+  }
+}

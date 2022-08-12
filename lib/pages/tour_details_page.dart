098b58d087db4416e22328e91d34ecb95474ebb4
Beka Dessalegn <bekadessalegn@gmail.com>
2022-08-13 02:12:58 +0300

    my commit

--- lib/pages/tour_details_page.dart
+++ lib/pages/tour_details_page.dart
@@ -6,6 +6,10 @@ import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:iconify_flutter/icons/uiw.dart';
 import 'package:iconify_flutter/icons/ic.dart';
 import 'package:iconify_flutter/icons/charm.dart';
+import 'package:iconify_flutter/icons/jam.dart';
+import 'package:iconify_flutter/icons/ion.dart';
+import 'package:iconify_flutter/icons/uil.dart';
+import 'package:iconify_flutter/icons/cil.dart';
 
 class TourDetailsPage extends StatefulWidget {
   const TourDetailsPage({Key? key}) : super(key: key);
@@ -19,47 +23,54 @@ class _TourDetailsPageState extends State<TourDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(kToolbarHeight),
-        child: Container(
-          height: 80,
-          decoration: BoxDecoration(boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              offset: Offset(0, 1.0),
-              blurRadius: 7.0,
-            )
-          ],
-              color: Colors.white),
-          child: AppBar(
-              brightness: Brightness.dark,
-              backgroundColor: Colors.transparent,
-              elevation: 0.0,
-              toolbarHeight: 70,
-              automaticallyImplyLeading: false,
-              actions: [
-                IconButton(
+        resizeToAvoidBottomInset: true,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                offset: Offset(0, 1.0),
+                blurRadius: 7.0,
+              )
+            ], color: Colors.white),
+            child: AppBar(
+                brightness: Brightness.dark,
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+                toolbarHeight: 70,
+                automaticallyImplyLeading: false,
+                actions: [
+                  IconButton(
                     padding: EdgeInsets.fromLTRB(0, 0, 25, 0),
                     iconSize: 30,
-                    onPressed: () {
-                    },
-                    icon: Icon(
-                      Icons.add_shopping_cart,
-                      color: Colors.black,
-                    )),
-                IconButton(
-                    padding: EdgeInsets.fromLTRB(0, 0, 25, 0),
-                    iconSize: 30,
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => SigninPage()));
-                    },
-                    icon: Iconify(Uiw.user)),
-              ],
-              title: Image.asset("images/group2_blue.png", scale: 1.3,)
+                    onPressed: () {},
+                    icon: Iconify(
+                      Jam.shopping_cart,
+                      size: 28,
+                    ),
+                  ),
+                  IconButton(
+                      padding: EdgeInsets.fromLTRB(0, 0, 25, 0),
+                      iconSize: 30,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SigninPage()));
+                      },
+                      icon: Iconify(
+                        Uiw.user,
+                        size: 22,
+                      )),
+                ],
+                title: Image.asset(
+                  "images/logo_small.png",
+                  scale: 8.3,
+                )),
           ),
         ),
-      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -158,6 +169,7 @@ class _TourDetailsPageState extends State<TourDetailsPage> {
                   margin: EdgeInsets.only(top: 20, right: 10, left: 10),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Column(
                         children: [
@@ -196,17 +208,17 @@ class _TourDetailsPageState extends State<TourDetailsPage> {
                                   borderRadius: BorderRadius.circular(8),
                                   border: Border.all(color: Colors.black54),
                                 ),
-                                child: Iconify(Ic.outline_watch_later, size: 25, color: Colors.black54,),
+                                child: Iconify(Uil.users_alt, size: 25, color: Colors.black54,),
                               ),
                               SizedBox(width: 10,),
                               Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text("Duration", style: TextStyle(
+                                  Text("Group Size", style: TextStyle(
                                       color: Colors.black,
                                       fontSize: 18
                                   ),),
-                                  Text("6 hours", style: TextStyle(
+                                  Text("6 people", style: TextStyle(
                                       color: Colors.black54,
                                       fontSize: 15
                                   ))
@@ -226,17 +238,18 @@ class _TourDetailsPageState extends State<TourDetailsPage> {
                                   borderRadius: BorderRadius.circular(8),
                                   border: Border.all(color: Colors.black54),
                                 ),
-                                child: Iconify(Ic.outline_watch_later, size: 25, color: Colors.black54,),
+
+                                child: Iconify(Ion.footsteps_outline, size: 25, color: Colors.black54,),
                               ),
                               SizedBox(width: 10,),
                               Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text("Duration", style: TextStyle(
+                                  Text("Tour Type", style: TextStyle(
                                       color: Colors.black,
                                       fontSize: 18
                                   ),),
-                                  Text("6 hours", style: TextStyle(
+                                  Text("Daily Tour", style: TextStyle(
                                       color: Colors.black54,
                                       fontSize: 15
                                   ))
@@ -246,6 +259,7 @@ class _TourDetailsPageState extends State<TourDetailsPage> {
                           ),
                           SizedBox(height: 15,),
                           Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Container(
                                 padding: EdgeInsets.all(8),
@@ -253,20 +267,32 @@ class _TourDetailsPageState extends State<TourDetailsPage> {
                                   borderRadius: BorderRadius.circular(8),
                                   border: Border.all(color: Colors.black54),
                                 ),
-                                child: Iconify(Ic.outline_watch_later, size: 25, color: Colors.black54,),
+                                child: Iconify(Cil.language, size: 25, color: Colors.black54,),
                               ),
                               SizedBox(width: 10,),
                               Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text("Duration", style: TextStyle(
+                                  Text("Languages", style: TextStyle(
                                       color: Colors.black,
                                       fontSize: 18
                                   ),),
-                                  Text("6 hours", style: TextStyle(
+                                  Text("Amharic,", style: TextStyle(
                                       color: Colors.black54,
                                       fontSize: 15
-                                  ))
+                                  )),
+                                  Text("English,", style: TextStyle(
+                                      color: Colors.black54,
+                                      fontSize: 15
+                                  )),
+                                  Text("Francais,", style: TextStyle(
+                                      color: Colors.black54,
+                                      fontSize: 15
+                                  )),
+                                  Text("Oromifa,", style: TextStyle(
+                                      color: Colors.black54,
+                                      fontSize: 15
+                                  )),
                                 ],
                               )
                             ],
@@ -361,43 +387,161 @@ class _TourDetailsPageState extends State<TourDetailsPage> {
                 SizedBox(height: 40,),
                 Divider(color: Colors.black,),
                 SizedBox(height: 20,),
-                Text("About this tour", style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w500
-                ),),
-                SizedBox(height: 10,),
-                Text("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.", style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black54,
-                    fontWeight: FontWeight.w500
-                ),),
-                SizedBox(height: 20,),
-                Divider(color: Colors.black,),
-                SizedBox(height: 30,),
-                Text("Tour's Location", style: TextStyle(
-                    fontSize: 26,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w500
-                ),),
-                SizedBox(height: 5,),
-                Row(
-                  children: [
-                    Icon(Icons.location_on_outlined, color: Colors.black54, size: 25,),
-                    Text(" Wilmington", style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black54
-                    ),)
-                  ],
-                ),
-                SizedBox(height: 20,),
                 Container(
-                  height: 500,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(image: AssetImage("images/tour_location.png"))
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("About this tour", style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500
+                      ),),
+                      SizedBox(height: 10,),
+                      Text("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.", style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w500
+                      ),
+                        textAlign: TextAlign.justify,
+                      ),
+                      SizedBox(height: 20,),
+                      Divider(color: Colors.black,),
+                      SizedBox(height: 30,),
+                      Text("Included/Excluded", style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500
+                      ),),
+                      SizedBox(height: 20,),
+                      Row(
+                        children: [
+                          Iconify(Ion.ios_checkmark_circle, color: Colors.green[200],),
+                          SizedBox(width: 10,),
+                          Text("Specialized bilingual guide", style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500
+                          ),),
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          Iconify(Ion.ios_checkmark_circle, color: Colors.green[200],),
+                          SizedBox(width: 10,),
+                          Text("Private Transport", style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500
+                          ),),
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          Iconify(Ion.ios_checkmark_circle, color: Colors.green[200],),
+                          SizedBox(width: 10,),
+                          Flexible(
+                            child: Text("Entrance fees (Cable and car and Moon Valley)", style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black54,
+                                fontWeight: FontWeight.w500
+                            ),),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          Iconify(Ion.ios_checkmark_circle, color: Colors.green[200],),
+                          SizedBox(width: 10,),
+                          Flexible(
+                            child: Text("Box lunch water, banana apple and chocolate", style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black54,
+                                fontWeight: FontWeight.w500
+                            ),),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          Iconify(Ic.baseline_cancel, color: Colors.red[200],),
+                          SizedBox(width: 10,),
+                          Text("Additional Services", style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500
+                          ),),
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          Iconify(Ic.baseline_cancel, color: Colors.red[200],),
+                          SizedBox(width: 10,),
+                          Text("Insurance", style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500
+                          ),),
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          Iconify(Ic.baseline_cancel, color: Colors.red[200],),
+                          SizedBox(width: 10,),
+                          Text("Drink", style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500
+                          ),),
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          Iconify(Ic.baseline_cancel, color: Colors.red[200],),
+                          SizedBox(width: 10,),
+                          Text("Tickets", style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w500
+                          ),),
+                        ],
+                      ),
+                      SizedBox(height: 20,),
+                      Divider(color: Colors.black,),
+                      SizedBox(height: 30,),
+                      Text("Tour's Location", style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500
+                      ),),
+                      SizedBox(height: 5,),
+                      Row(
+                        children: [
+                          Icon(Icons.location_on_outlined, color: Colors.black54, size: 25,),
+                          Text(" Wilmington", style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black54
+                          ),)
+                        ],
+                      ),
+                      SizedBox(height: 20,),
+                      Container(
+                        height: 500,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(image: AssetImage("images/tour_location.png"))
+                        ),
+                      ),
+                      SizedBox(height: 40,)
+                    ],
                   ),
                 ),
-                SizedBox(height: 80,)
               ],
             )
             ),

ff7b0d202c5a45f9332b62a818c0dfc2b42c6d47
Estifanos Neway <estifanos.neway.d@gmail.com>
2022-08-13 02:16:51 +0300

    s

--- lib/data/hotels.dart
+++ lib/data/hotels.dart
@@ -1,17 +1,89 @@
 List<Map<String, dynamic>> hotels = [
   {
     "name": "Capital Hotel and Spa",
-    "imageUrl": "images/hotel1.jpg",
+    "imageUrl": "images/h1.jpg",
+    "location": "Addis Ababa",
+    "reviewCount": 15,
+    "starCount": 4.6,
+    "price": "200.00 ETB",
+    "ratingTag": "Excellent"
+  },
+  {
+    "name": "Elilly International Hotel",
+    "imageUrl": "images/h2.jpg",
+    "location": "Addis Ababa",
+    "reviewCount": 3,
+    "starCount": 5.0,
+    "price": "200.00 ETB",
+    "ratingTag": "Excellent"
+  },
+  {
+    "name": "Safari Lodge",
+    "imageUrl": "images/h3.jpg",
+    "location": "Adama",
+    "reviewCount": 23,
+    "starCount": 5.0,
+    "price": "200.00 ETB",
+    "ratingTag": "Excellent"
+  },
+  {
+    "name": "Atlas International Hotel",
+    "imageUrl": "images/h4.jpg",
+    "location": "Addis Ababa",
+    "reviewCount": 30,
+    "starCount": 2.9,
+    "price": "200.00 ETB",
+    "ratingTag": "Fair"
+  },
+  {
+    "name": "Sheraton Addis Hotel",
+    "imageUrl": "images/h5.jpg",
     "location": "Addis Ababa",
     "reviewCount": 5,
+    "starCount": 5.0,
+    "price": "200.00 ETB",
+    "ratingTag": "Excellent"
+  },
+  {
+    "name": "Bole Ambassador Hotel",
+    "imageUrl": "images/h6.jpg",
+    "location": "Addis Ababa",
+    "reviewCount": 13,
+    "starCount": 5.0,
+    "price": "200.00 ETB",
+    "ratingTag": "Excellent"
+  },
+  {
+    "name": "Adma German Hotel",
+    "imageUrl": "images/h7.jpg",
+    "location": "Delaware",
+    "reviewCount": 25,
+    "starCount": 4.3,
+    "price": "200.00 ETB",
+    "ratingTag": "Good"
+  },
+  {
+    "name": "Abyssinia Renaissance Hotel",
+    "imageUrl": "images/h8.jpg",
+    "location": "Addis Ababa",
+    "reviewCount": 50,
+    "starCount": 3.8,
+    "price": "200.00 ETB",
+    "ratingTag": "Good"
+  },
+  {
+    "name": "Grand Yordanos Hotel",
+    "imageUrl": "images/h9.jpg",
+    "location": "Addis Ababa",
+    "reviewCount": 18,
     "starCount": 4.0,
     "price": "200.00 ETB",
     "ratingTag": "Good"
   },
   {
-    "name": "Capital Hotel and Spa",
-    "imageUrl": "images/hotel1.jpg",
-    "location": "Addis Ababa",
+    "name": "Honeyland Hotel",
+    "imageUrl": "images/h10.jpg",
+    "location": "Adama",
     "reviewCount": 3,
     "starCount": 5.0,
     "price": "200.00 ETB",
